afb7b2d74b665c63972256380681ccf3ac6d0a32
Klaudia <56263298+klaudiajaros@users.noreply.github.com>
2021-02-24 19:03:07 +0000

    updated MainActivity, created SettingsActivity, added Polish translation for some strings

--- app/src/main/java/com/napier/sunlightapp/MainActivity.java
+++ app/src/main/java/com/napier/sunlightapp/MainActivity.java
@@ -9,6 +9,8 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -16,8 +18,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,11 +38,24 @@ public class MainActivity extends AppCompatActivity {
     private LocationListener locationListener;
     // The identifier for which permission request has been answered.
     private static final int ACCESS_REQUEST_LOCATION = 0;
+    private double lat;
+    private double lon;
+    private String currentCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button settings = (Button) findViewById(R.id.settingsButton);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settingsActivity = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(settingsActivity);
+            }
+        });
+
 
         // Acquire a reference to the system Location Manager
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -43,7 +68,12 @@ public class MainActivity extends AppCompatActivity {
                             "Location changed: Lat: " + location.getLatitude() +
                                     " Lng: " + location.getLongitude();
 
+                    lat = location.getLatitude();
+                    lon = location.getLongitude();
+                    System.out.println("on create: lon and lat: "+lon + " " + lat);
+
                     Toast.makeText(getBaseContext(), locationString, Toast.LENGTH_LONG).show();
+                    setCity();
                 }
             }
 
@@ -63,6 +93,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+
+
         // Handle the case we don't have the necessary permission
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
@@ -78,6 +110,45 @@ public class MainActivity extends AppCompatActivity {
                     "Location is allowed.");
             setLocationUpdateFunction();
         }
+
+
+
+        /*try {
+            connectWeatherAPI();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    /*
+    setCity method sets the current city based on the location and changes the locationValue string in Main Activity
+     */
+    private void setCity(){
+
+        System.out.println("lon and lat: "+lon + " " + lat); // for debugging purposes
+
+        TextView locationTextView = findViewById(R.id.locationValue); // find the current city string
+
+        Geocoder gcd = new Geocoder(this, Locale.getDefault()); // using Geocoder class to find the current city based on longitude and latitude
+        List<Address> addresses = null;
+        try {
+            addresses = gcd.getFromLocation(lat, lon, 10); // get a sample of addresses
+            if (addresses.size() > 0) { // if not empty
+                for (Address a: addresses) { // loop through addresses to find a city
+                    if (a.getLocality() != null && a.getLocality().length() > 0) { // if not null or empty
+                        currentCity=a.getLocality(); // save the location
+                        locationTextView.setText(currentCity);  // change the city in the main activity
+                        break;
+                    }
+                }
+            }
+            else {
+                locationTextView.setText("No address found");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -114,5 +185,23 @@ public class MainActivity extends AppCompatActivity {
         locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER,
                 MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListener);
     }
+
+
+    /*private void connectWeatherAPI() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request APIrequest = new Request.Builder()
+                .url("https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html")
+                .get()
+                .addHeader("x-rapidapi-key", "5471ed4925msh3fd6adab960ceddp1fa57fjsnfd08b578b125")
+                .addHeader("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
+                .build();
+
+        Response APIresponse = client.newCall(APIrequest).execute();
+
+        TextView weatherData = findViewById(R.id.weatherDataView);
+        //weatherData.setText(APIresponse.toString());
+
+    }*/
 
 }
